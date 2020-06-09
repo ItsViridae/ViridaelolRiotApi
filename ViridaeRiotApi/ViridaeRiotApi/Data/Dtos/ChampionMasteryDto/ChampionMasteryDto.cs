333a4a315a7fdc40bbbbc19a5a998cf2413a31c4
Scott Comeaux <viridaecode@gmail.com>
2020-06-08 20:49:28 -0500

    feat: Champion Mastery Dto/Entity

---
+++ ViridaeRiotApi/ViridaeRiotApi/Data/Dtos/ChampionMasteryDto/ChampionMasteryDto.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViridaeRiotApi.Data.Dtos.ChampionMasteryDto
+{
+    public class ChampionMasteryDto
+    {
+        public int Id { get; set; }
+        [JsonProperty("championId")]
+        public int ChampionId { get; set; }
+        public string ChampionName { get; set; }
+        [JsonProperty("championLevel")]
+        public int ChampionLevel { get; set; }
+        [JsonProperty("summonerId")]
+        public int SummonerId { get; set; }
+    }
+}
---
+++ ViridaeRiotApi/ViridaeRiotApi/Data/Entities/ChampionMasteryEntities/ChampionMastery.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViridaeRiotApi.Data.Entities.ChampionMasteryEntities
+{
+    public class ChampionMastery
+    {
+        public int Id { get; set; }
+        [JsonProperty("championId")]
+        public int ChampionId { get; set; }
+        public string ChampionName { get; set; }
+        [JsonProperty("championLevel")]
+        public int ChampionLevel { get; set; }
+        [JsonProperty("summonerId")]
+        public int SummonerId { get; set; }
+        public Summoner Summoner { get; set; }
+    }
+}
